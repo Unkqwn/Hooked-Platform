7d50824fca1fa8f158e6de3f5a1085c31ceec242
Unkqwn <Unkqwn@hotmail.com>
2026-07-14 21:13:48 +0200

    Movement updated with acceleration and deceleration

--- Assets/Robin/Scripts/Movement.cs
+++ Assets/Robin/Scripts/Movement.cs
@@ -5,7 +5,10 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float baseSpeed = 5f;
+    [SerializeField] private float acceleration = 20f;
+    [SerializeField] private float deceleration = 12f;
 
+    private Rigidbody rb;
     private Vector2 moveInput;
 
     public void OnMove(InputAction.CallbackContext context)
@@ -13,19 +16,35 @@ public class Movement : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
     }
 
-    void Update()
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
     {
         HandleMovement();
     }
 
     private void HandleMovement()
     {
-        float speed = baseSpeed;
+        Vector3 moveDirection = new Vector3(moveInput.x, 0, moveInput.y).normalized;
+        Vector3 currentVelocity = rb.linearVelocity;
+        Vector3 targetVelocity = moveDirection * baseSpeed;
+        
+        float rate;
+        if (moveDirection.sqrMagnitude > 0f)
+        {
+            rate = acceleration;
+        }
+        else
+        {
+            rate = deceleration;
+        }
 
-        float horizontal = moveInput.x;
-        float vertical = moveInput.y;
+        currentVelocity.x = Mathf.MoveTowards(currentVelocity.x, targetVelocity.x, rate * Time.fixedDeltaTime);
+        currentVelocity.z = Mathf.MoveTowards(currentVelocity.z, targetVelocity.z, rate * Time.fixedDeltaTime);
 
-        Vector3 movement = new Vector3(horizontal, 0f, vertical) * speed * Time.deltaTime;
-        transform.Translate(movement, Space.World);
+        rb.linearVelocity = currentVelocity;
     }
 }
